5521ee9f60868f0cd2518ef7c1c8407de0fabece
Yuhui <130331257+yuhuicaoo@users.noreply.github.com>
2023-04-25 16:43:59 +1200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,12 +31,16 @@ const Home = ({
   trendingNow,
 }: Props) => {
   const { loading } = useAuth();
-  const showModal = useRecoilValue(modalState)
+  const showModal = useRecoilValue(modalState);
 
   if (loading) return null;
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
