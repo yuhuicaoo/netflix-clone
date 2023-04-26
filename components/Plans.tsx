51dacd2cbb16a12585f375dece429d0e8e5018bb
Yuhui <130331257+yuhuicaoo@users.noreply.github.com>
2023-04-27 00:11:27 +1200

    started day3 of netflix clone
    started day3 , little bit of coding on the plans page

---
+++ components/Plans.tsx
@@ -0,0 +1,56 @@
+import useAuth from "@/hooks/useAuth";
+import { CheckIcon } from "@heroicons/react/solid";
+import Head from "next/head";
+import Link from "next/link";
+
+function Plans() {
+  const { logout } = useAuth();
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="border-b border-white/10 bg-[#141414]">
+        <Link href="/">
+          <img
+            src="https://rb.gy/ulxxee"
+            alt="Netflix"
+            width={150}
+            height={90}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <button
+          className="text-lg font-medium hover:underline"
+          onClick={logout}
+        >
+          Sign Out
+        </button>
+      </header>
+
+      <main className="pt-28">
+        <h1 className="mb-3 text-3xl font-medium">
+          Choose the plan that's right for you
+        </h1>
+        <ul>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+            Ad-free.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
+            just for you.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
+            your plan anytime.
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default Plans;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { modalState } from "@/atoms/modalAtom";
 import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import Modal from "@/components/Modal";
+import Plans from "@/components/Plans";
 import Row from "@/components/Row";
 import useAuth from "@/hooks/useAuth";
 import { Movie } from "@/typings";
@@ -32,8 +33,11 @@ const Home = ({
 }: Props) => {
   const { loading } = useAuth();
   const showModal = useRecoilValue(modalState);
+  const sbuscription = false
 
-  if (loading) return null;
+  if (loading || sbuscription === null) return null;
+
+  if (!sbuscription) return <Plans />
 
   return (
     <div
